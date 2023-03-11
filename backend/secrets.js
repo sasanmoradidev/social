5c674cc8214a7bc09c7693693ae93f80c58f09af
moradilahroodi <ssn.moradi@gmail.com>
2023-03-11 16:09:30 +0330

    initial

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,3 +1,4 @@
 module.exports = {
-  dbURI: 'TODO: Replace with your MongoDB connection string',
+  dbURI: 'mongodb://localhost:27017/social',
+  jwtKey:'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY3ODUzMDQ2OCwiaWF0IjoxNjc4NTMwNDY4fQ.1xjL9jpJv0Iu-oIDO6-Jz1e2pJ_EEbR-8rePJj_74_U'
 };
